a5ef6e9b588133179610a83321c6ad87f40fb90d
Mohammad Morsalin Hossain <mdmorsalin80@gmail.com>
2019-02-20 17:07:00 +0600

    Add solution for problem `A - Hexadecimal's theorem` Link: https://codeforces.com/contest/199/problem/A Tags: brute force, constructive algorithms, implementation, number theory, *900 Ref: https://codeforces.com/contest/199/submission/50236810

--- codeforces/199/A.cpp
+++ codeforces/199/A.cpp
@@ -10,7 +10,25 @@ int main()
 	IOS
     ll n;
     while(cin>>n){
-    cout << "0 0 "<<n<<endl;
+    ll beg=0,pre=1,now;
+    vector<int> vec;
+    if(n==0){ cout << "0 0 0\n";continue;}
+    vec.pb(0);vec.pb(1);
+    while(1)
+    {
+        now = beg+pre;
+        vec.pb(now);
+        if(now==n) break;
+        beg = pre; pre =now;
+    }
+    if(n==2) cout << "0 1 1\n";
+    else if(n==1) cout << "0 0 1\n";
+    else if(n==3) cout << "1 1 1\n";
+
+    else{
+        int sz  = vec.size();
+        cout  << vec[sz-5]<< " " << vec[sz-4] << " " << vec[sz-2] <<endl;
+    }
     }
     return 0;
 }
