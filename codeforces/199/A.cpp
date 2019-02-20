6acbe6fd0eb01feff5d879919372ab06c6202e71
Mohammad Morsalin Hossain <mdmorsalin80@gmail.com>
2019-02-20 17:09:00 +0600

    Add solution for problem `A - Hexadecimal's theorem` Link: https://codeforces.com/contest/199/problem/A Tags: brute force, constructive algorithms, implementation, number theory, *900 Ref: https://codeforces.com/contest/199/submission/50236871

---
+++ codeforces/199/A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ms(x) memset(x,0,sizeof(x))
+#define IOS ios::sync_with_stdio(false);
+using namespace std;
+int main()
+{
+	IOS
+    ll n;
+    while(cin>>n){
+    cout << "0 0 "<<n<<endl;
+    }
+    return 0;
+}
+
