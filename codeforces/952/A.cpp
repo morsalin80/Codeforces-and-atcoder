14b35a559a1609a8c058536d8d96a3ff961bc480
Mohammad Morsalin Hossain <mdmorsalin80@gmail.com>
2019-03-01 14:53:00 +0600

    Add solution for problem `A - Quirky Quantifiers` Link: https://codeforces.com/contest/952/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/952/submission/50630518

---
+++ codeforces/952/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ms(x) memset(x,0,sizeof(x))
+#define ins insert
+#define IOS ios::sync_with_stdio(false);
+using namespace std;
+int main()
+{
+	IOS
+    int n;
+    while(cin>>n)
+    {
+        cout << n%2 <<endl;
+    }
+    return 0;
+}
+
