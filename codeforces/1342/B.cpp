bb722671b96ca2ef4e55eb44b46bdc7488ce54a0
Mohammad Morsalin Hossain <mdmorsalin80@gmail.com>
2020-04-26 21:41:00 +0600

    Add solution for problem `B - Binary Period` Link: https://codeforces.com/contest/1342/problem/B Tags: constructive algorithms, strings, *1100 Ref: https://codeforces.com/contest/1342/submission/78182413

---
+++ codeforces/1342/B.cpp
@@ -0,0 +1,75 @@
+/// Bismillahir Rahmanir Rahim
+/* Author: Mohammad Morsalin (aka white_space)
+    Dept of ICE, NSTU
+*/
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define endl "\n"
+#define f0(n) for(int i=0;i<n;i++)
+#define ms(x) memset(x,0,sizeof(x))
+#define ins insert
+#define ALL(v) v.begin(),v.end()
+#define highest(x) numeric_limits<x>::max()
+#define lowest(x) numeric_limits<x>::min()
+#define Inf INFINITY
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define PI acos(-1)
+#define IOS ios::sync_with_stdio(false);
+using namespace std;
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[] = {0, 0, 1, -1};
+int dy4[] = {1, -1, 0, 0};
+const long long MOD = 1000000007;
+template<typename T>inline T Bigmod(T base, T power, T MOD){
+    T ret=1;
+    while(power)
+    {
+        if(power & 1)ret=(ret*base)%MOD;
+        base=(base*base)%MOD;
+        power>>=1;
+    }
+    return ret;
+}
+
+bool sortinrev(const pair<int,int> &a,
+               const pair<int,int> &b)
+{
+       return (a.first > b.first);
+}
+int main()
+{
+    IOS
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        set<char>st;
+        int n=s.size();
+        bool one=0,z=0;
+        for(int i=0;i<(int)s.size();i++){
+            if(s[i]=='0')z=1;
+            else one=1;
+        }
+
+        //cout << st.size()<<endl;
+        if(!z || !one){ cout << s << endl;continue;}
+        for(int i=0;i<(int)s.size();i++){
+            cout << s[i];
+            if(s[i]=='0' && i<n-1 && s[i+1]!='1'){
+                cout <<'1';
+            }
+            else if(s[i]=='1' && i<n-1 && s[i+1]!='0'){
+                cout <<'0';
+            }
+        }
+        cout << endl;
+
+    }
+    return 0;
+}
+///Alhamdulillah
