56db8fd7c13b7c0fd2c2e0b813df9a5e536f8229
Mohammad Morsalin Hossain <mdmorsalin80@gmail.com>
2020-05-23 08:14:00 +0600

    Add solution for problem `B - Kuriyama Mirai's Stones` Link: https://codeforces.com/contest/433/problem/B Tags: dp, implementation, sortings, *1200 Ref: https://codeforces.com/contest/433/submission/80978154

---
+++ codeforces/433/B.cpp
@@ -0,0 +1,76 @@
+/// Bismillahir Rahmanir Rahim
+/* Author: Mohammad Morsalin (aka white_space)
+    Dept of ICE, NSTU
+*/
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define endl "\n"
+#define f0(n) for(int i=0;i<n;i++)
+#define ms(x) memset(x,0,sizeof(x))
+#define ins insert
+#define ALL(v) v.begin(),v.end()
+#define highest(x) numeric_limits<x>::max()
+#define lowest(x) numeric_limits<x>::min()
+#define Inf INFINITY
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define PI acos(-1)
+#define IOS ios::sync_with_stdio(false);
+using namespace std;
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[] = {0, 0, 1, -1};
+int dy4[] = {1, -1, 0, 0};
+const long long MOD = 1000000007;
+template<typename T>inline T Bigmod(T base, T power, T MOD){
+    T ret=1;
+    while(power)
+    {
+        if(power & 1)ret=(ret*base)%MOD;
+        base=(base*base)%MOD;
+        power>>=1;
+    }
+    return ret;
+}
+
+bool sortinrev(const pair<int,int> &a,
+               const pair<int,int> &b)
+{
+       return (a.first > b.first);
+}
+int main()
+{
+    IOS
+    ll n;
+    while(cin>>n){
+        ll arr[n+1];
+        ll ans1[n+1];
+        ans1[0]=0;
+        arr[0]=0;
+        for(int i=1;i<=n;i++){
+            cin>>arr[i];
+            ans1[i]=ans1[i-1]+arr[i];
+        }
+        sort(arr,arr+n+1);
+        for(int i=1;i<=n;i++){
+            arr[i]+=arr[i-1];
+        }
+        //cout << endl;
+        int m;
+        cin>>m;
+        while(m--){
+            int t,l,r;
+            cin>>t>>l>>r;
+            if(t==1){
+                cout << ans1[r]-ans1[l-1] << endl;
+            }
+            else{
+                cout << arr[r]-arr[l-1] << endl;
+            }
+        }
+    }
+    return 0;
+}
+///Alhamdulillah
