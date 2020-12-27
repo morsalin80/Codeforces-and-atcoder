12be88fc278b37b6acc3ce7170f045936ad9a712
Mohammad Morsalin Hossain <mdmorsalin80@gmail.com>
2020-12-27 23:10:00 +0600

    Add solution for problem `C - Bear and Prime Numbers` Link: https://codeforces.com/contest/385/problem/C Tags: binary search, brute force, data structures, dp, implementation, math, number theory, *1700 Ref: https://codeforces.com/contest/385/submission/102488852

--- codeforces/385/C.cpp
+++ codeforces/385/C.cpp
@@ -1,64 +1,94 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int maxn=1e4;
-bitset<10000010>num;
-vector<int>prime;
-void seive(){
-    num.set();
-    prime.push_back(2);
-    num[0]=num[1]=0;
-    for(int i=2;i<=maxn;i+=2){
-        num[i]=0;
-    }
-    for(int i=3;i<=maxn;i+=2){
-        if(num[i]){
-            prime.push_back(i);
-            for(int j=i*i;j<=maxn;j+=(i+i)){
-                num[j]=0;
+const long long MAXN = 1e4;
+
+
+bitset<10000010> bs;
+vector<int> primes;
+
+void sieve()
+{
+    bs.set();
+    bs[0]=bs[1]=0;
+    primes.push_back(2);
+
+    for(int i=2; i<=MAXN ; i+=2)
+        bs[i]=0;
+
+    for(int i=3; i<=MAXN ; i+=2)
+    {
+        if(bs[i])
+        {
+            primes.push_back(i);
+            for(int j = i*i ; j<=MAXN ; j+=(i+i))
+            {
+                bs[j]=0;
             }
         }
     }
 }
-int pre[(int)1e7+1]={0};
-void factor(int x){
-    int xx=x;
-    for(int i=0; i<(int)prime.size() && prime[i]*prime[i]<=xx;i++){
-        int p=prime[i],cnt=0;
-        if(p>x)break;
-        while(x%p==0){
-            x/=p;
-            cnt++;
+
+int pref[(int)1e7+1]= {0};
+void Factor(int n)
+{
+    int nn=n;
+    for(int i=0; i<primes.size() && primes[i]*primes[i]<=nn ; i++)
+    {
+        int p = primes[i],freq=0;
+        if(p>n)
+            break;
+        while(n%p==0)
+        {
+            n/=p;
+            freq=1;
         }
-        if(cnt!=0){
-            pre[p]++;
-        }
+        if(freq)
+            pref[p]++;
     }
-    if(x>1)pre[x]++;
+    if(n>1)
+        pref[n]++;
 }
-int main(){
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    seive();
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    sieve();
+
     long long n,x;
-    cin>>n;
-    for(int i=0;i<n;i++){
+    cin >> n;
+
+    for(long long i=0; i<n; i++)
+    {
         cin>>x;
-        factor(x);
+        Factor(x);
     }
-    for(int i=1;i<=1e7;i++){
-        pre[i]+=pre[i-1];
+
+    for(int i=1;i<=1e7 ;i++){
+         pref[i]+=pref[i-1];
     }
-    int q;
-    cin>>q;
-    while(q--){
+
+    long long q;
+    cin >> q;
+
+    while(q--)
+    {
         long long l,r;
-        cin>>l>>r;
-        if(l>=1e7){
-            cout << 0<<endl;
+        cin >>  l >>r;
+
+        if(l>=1e7)
+            cout<<0<<endl;
+        else if(r>1e7)
+        {
+            cout<<pref[(int)1e7]-pref[l-1]<<endl;
         }
-        else if(r>1e7){
-            cout<<pre[(int)1e7]-pre[l-1]<<endl;
+        else
+        {
+            cout<<pref[r]-pref[l-1]<<endl;
         }
-        else cout<<pre[r]-pre[l-1]<<endl;
+
     }
-    return 0;
+
+
 }
